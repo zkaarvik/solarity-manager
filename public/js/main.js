a1d9dec8d65c084bc701a5b04f0ac74c319555b1
Zach <zkaarvik@gmail.com>
2015-10-18 15:46:01 -0700

    Zach: Add UI libraries with Bower, and create simple Angular UI

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+//Declare the module
+var solarityManager = angular.module('solarityManager', ['ui.bootstrap']);
+
+//Configure module
+
+solarityManager.controller('MainController', function($scope) {
+	$scope.buttonModel = true;
+});
