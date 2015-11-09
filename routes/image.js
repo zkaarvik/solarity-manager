a353b42db8af915883b999a2711428f747c3c2c5
Zachary Kaarvik <zkaarvik@gmail.com>
2015-11-08 20:01:57 -0800

    Zach: Image conversion progress. Have array of pixels. Need to convert to EPD

--- routes/image.js
+++ routes/image.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var request = require('request');
 var gm = require('gm');
+var ByteBuffer = require('bytebuffer');
+var PNG = require('png-js');
 
 /* GET /image/id */
 /*
@@ -28,7 +30,6 @@ router.get('/:id', function(req, res, next) {
 
             //Draw title
             var image = gm(iWidth, iHeight, '#FFFFFF')
-                .channel('black')
                 .font('impact', 96)
                 .drawText(10, 96, 'Solarity');
 
@@ -56,44 +57,64 @@ router.get('/:id', function(req, res, next) {
                 iVerticalLength += 60;
             }
 
-            convertImage(image);
+            //Convert image to black and white before converting to EPD
+            image.monochrome();
+
+            image.write('/tempSolarityImg/' + sDeviceID + '.png', function(err) {
+                if (err) console.log(err);
+
+                //Decode image into array of pixels. This array will be converted into EPD format
+                PNG.decode('/tempSolarityImg/' + sDeviceID + '.png', convertImage);
+            });
+            //res.set('Content-Type', 'image/png');
+            //image.stream('png').pipe(res);
         } else {
             res.send('Error requesting translink API');
         }
     };
 
     //Convert a gdImage to EPD format
-    function convertImage(image) {
+    function convertImage(pixels) {
+        //ByteBuffer size = EPD Image Size = 48,016 bytes
+        var buffer = ByteBuffer.allocate(48016);
+
+        //Write image header to buffer
+        buffer.writeUint8(0x3A);
+        buffer.writeUint8(0x01);
+        buffer.writeUint8(0xE0);
+        buffer.writeUint8(0x03);
+        buffer.writeUint8(0x20);
+        buffer.writeUint8(0x01);
+        buffer.writeUint8(0x04);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+        buffer.writeUint8(0x00);
+
         var x, y;
         var aLine;
         var aLines = [];
 
         //Parse the image one row at a time
-        for(y=0; y<iHeight; y++) {
-            aLine = [];
-            for(x=0; x<iWidth; x++) {
-                //aLine.push(image.imageColorAt(x, y));
-            }
-            aLines.push(aLine);
-        }
+        // for(y=0; y<iHeight; y++) {
+        //     aLine = [];
+        //     for(x=0; x<iWidth; x++) {
+        //         //aLine.push(image.imageColorAt(x, y));
+        //     }
+        //     aLines.push(aLine);
+        // }
+        res.send(pixels);
 
+        
 
-
-
-        res.set('Content-Type', 'image/png');
-        image.stream('png', function(err, stdout, stderr) {
-            if(err) console.log(err);
-            stdout.pipe(res);
-        });
-        // .write('/tempSolarityImg/' + sDeviceID + '.png', function(err) {
-        //     if (err) console.log(err);
-        //     //else res.send('noerr');
-        // });
-    };
-
-
-    //res.set('Content-Type', 'image/png');
-    
+        //buffer.flip();
+        //console.log(buffer.readUint8());
+    };  
 
 });
 
